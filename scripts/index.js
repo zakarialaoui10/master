8759c341bb11f23911ce177ba58ada73c56fcf74
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-09-04 16:00:10 +0100

    test custom msg

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ import {writeFileSync, existsSync, mkdirSync} from 'fs'
 const owner = process.env.OWNER;
 const repo = process.env.REPO;
 const BaseDir = process.cwd();
+const custom = process.env.CUSTOM;
 
 
 console.log({
@@ -9,7 +10,8 @@ console.log({
     repo,
     BaseDir,
     t : Date.now(),
-    a : 0
+    a : 0,
+    custom
 })
 
 const path = './cache';
@@ -23,5 +25,6 @@ writeFileSync('./cache/test.json', JSON.stringify({
     repo,
     BaseDir,
     t : Date.now(),
-    a : 0
+    a : 0,
+    custom
 }));
